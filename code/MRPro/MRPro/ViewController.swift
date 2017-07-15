a44ea09391af4ad81c2345a65138146dd940d0c4
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-07-15 15:51:32 +0100

    Initial Code refactoring some code again to make the project much more better and make improvements

---
+++ code/MRPro/MRPro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MRPro
+//
+//  Created by Nav on 15/07/2017.
+//  Copyright © 2017 MeetingRoom Pro | Navjot Singh Virk | Gymandnutrition.com | Navsingh.org.uk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ code/MRPro/MRProTests/MRProTests.swift
@@ -0,0 +1,36 @@
+//
+//  MRProTests.swift
+//  MRProTests
+//
+//  Created by Nav on 15/07/2017.
+//  Copyright © 2017 MeetingRoom Pro | Navjot Singh Virk | Gymandnutrition.com | Navsingh.org.uk. All rights reserved.
+//
+
+import XCTest
+@testable import MRPro
+
+class MRProTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
