ff8a6608d1a7d46f3b9bee2a359e8bc9718c72b9
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-07-15 19:51:02 +0100

    companies table added and some changes to buildings table

--- database/table.sql
+++ database/table.sql
@@ -1,28 +1,39 @@
-CREATE TABLE `mrp_buildings` (
-  `buildingId` int(120) NOT NULL,
-  `name` varchar(120) NOT NULL,
-  `numberOfFloors` int(120) NOT NULL,
-  `coordinates` geometry NOT NULL,
-  `city` varchar(120) NOT NULL,
-  `country` varchar(120) NOT NULL,
-  `address` varchar(240) NOT NULL,
-  `phone` varchar(120) NOT NULL,
-  `email` varchar(120) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-ALTER TABLE `mrp_buildings`
-ADD PRIMARY KEY (`buildingId`);
+CREATE TABLE `navsingh_MRPro`.`mrp_companies` (
+    `companyId` INT(120) NOT NULL ,
+    `name` VARCHAR(240) NOT NULL ,
+    `headquarters` VARCHAR(480) NOT NULL ,
+    `website` VARCHAR(120) NOT NULL ,
+    `email` VARCHAR(120) NOT NULL ,
+    `ceo` VARCHAR(120) NOT NULL ,
+    `industry` VARCHAR(120) NOT NULL ,
+    `tagline` VARCHAR(120) NOT NULL , PRIMARY KEY (`companyId`)) ENGINE = MyISAM;
 
 
 
 
-CREATE TABLE `navsingh_MRPro`.`mrp_meeting_rooms` ( 
-  `meetingRoomId`INT(120) NOT NULL ,
-  `buildingId` INT(120) NOT NULL , 
-  `name` VARCHAR(120)NOT NULL , 
-  `floorNumber` INT(120) NOT NULL , 
-  `coordinates` POINT NOTNULL , 
-  `capacity` INT(120) NOT NULL , 
-  `type` VARCHAR(120) NOT NULL ,
-  `phone` VARCHAR(120) NOT NULL , 
-  `email` VARCHAR(120) NOT NULL ,
-  `directions` VARCHAR(480) NOT NULL , PRIMARY KEY (`meetingRoomId`))ENGINE = MyISAM;
+CREATE TABLE `navsingh_MRPro`.`mrp_buildings` (
+  `buildingId` INT(120) NOT NULL ,
+  `companyId` INT(120) NOT NULL ,
+  `name` VARCHAR(120) NOT NULL ,
+  `noOfFloors` INT(120) NOT NULL ,
+  `coordinates` POLYGON NOT NULL ,
+  `city` VARCHAR(120) NOT NULL ,
+  `country` VARCHAR(120) NOT NULL ,
+  `address` VARCHAR(240) NOT NULL ,
+  `phone` VARCHAR(120) NOT NULL ,
+  `email` VARCHAR(120) NOT NULL , PRIMARY KEY (`buildingId`)) ENGINE = MyISAM;
+
+
+
+
+  CREATE TABLE `navsingh_MRPro`.`mrp_meeting_rooms` ( 
+    `meetingRoomId`INT(120) NOT NULL ,
+    `buildingId` INT(120) NOT NULL , 
+    `name` VARCHAR(120)NOT NULL , 
+    `floorNumber` INT(120) NOT NULL , 
+    `coordinates` POINT NOT NULL , 
+    `capacity` INT(120) NOT NULL , 
+    `type` VARCHAR(120) NOT NULL ,
+    `phone` VARCHAR(120) NOT NULL , 
+    `email` VARCHAR(120) NOT NULL ,
+    `directions` VARCHAR(480) NOT NULL , PRIMARY KEY (`meetingRoomId`))ENGINE = MyISAM;
