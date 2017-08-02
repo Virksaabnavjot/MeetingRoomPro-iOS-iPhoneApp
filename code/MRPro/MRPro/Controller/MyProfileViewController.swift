aa230c2ff7605eb3b1b0e058db1f965941a21e6f
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-08-02 21:27:20 +0100

    Code explanation aka. comments added for My Profile controller

--- code/MRPro/MRPro/Controller/MyProfileViewController.swift
+++ code/MRPro/MRPro/Controller/MyProfileViewController.swift
@@ -1,13 +1,16 @@
 //
 //  MyProfileViewController.swift
 //  MRPro
-//
+//  Purpose: Handles User Profile
 //  Created by Nav
 //  Copyright © 2017 MeetingRoom Pro | Navjot Singh Virk | Gymandnutrition.com | Navsingh.org.uk. All rights reserved.
 //
 
 import UIKit
 
+/*
+ Helps display user information in My Profile section of the app
+ */
 class MyProfileViewController: UIViewController {
     
     
@@ -18,17 +21,16 @@ class MyProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Profile"
-        //setting a custom color for the view
-        //view.backgroundColor = UIColor.white
+        
         myProfileScrollView.tintColor = UIColor.orange
         
+        // get user defaults - the ones we saved in memory when used loged in
         let userDict =  UserDefaults.standard.object(forKey: "userData") as! NSDictionary
 
+        //show the user details in my profile
         self.nameLbl.text = userDict["name"] as? String
         self.emailLbl.text = userDict["email"] as? String
 
-        
-        
     }
     
     override func didReceiveMemoryWarning() {
