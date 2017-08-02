9b69fa4d08898cdd2de7f3f860737077bdc14bc6
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-08-02 20:36:57 +0100

    Organising Groups in folders to MVC like user groups
    As Xcode shows the files in group and its really organised by all the
    file are actually stored in the actual project folder so, viewing the
    files in a normal file system other than Xcode looks very messy and
    unorganised. I have created new folders with the same name as our
    different groups like Model, View, Controller, JsonParser and set the
    groups file location to the folders. So, now everything is organised in
    MVC Patterns

--- code/MRPro/MRPro/MeetingRoomController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MeetingRoomController.swift
-//  MRPro
-//
-//  Created by Nav on 18/07/2017.
-//  Copyright © 2017 MeetingRoom Pro | Navjot Singh Virk | Gymandnutrition.com | Navsingh.org.uk. All rights reserved.
-//
-
-import Foundation
