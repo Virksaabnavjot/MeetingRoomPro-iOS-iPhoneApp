ad0b10269fbed9687a1e0d0fda1028bebb97a63e
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-08-02 22:46:45 +0100

    Code Explanation - Buildings Table view controller class

--- code/MRPro/MRPro/Controller/BuildingListTableViewController.swift
+++ code/MRPro/MRPro/Controller/BuildingListTableViewController.swift
@@ -1,34 +1,44 @@
 //
 //  BuildingListTableViewController.swift
 //  MRPro
-//
+//  Purpose: Helps to display buildings list and allow search
 //  Created by Nav
 //  Copyright © 2017 MeetingRoom Pro | Navjot Singh Virk | Gymandnutrition.com | Navsingh.org.uk. All rights reserved.
 //
 
 import Foundation
 import UIKit
-//import ChameleonFramework
 
+/*
+ Display buildings list and allow search
+ */
 class BuildingListTableViewController: UITableViewController, UISearchBarDelegate, UISearchResultsUpdating {
     
-   
+    //create buildings array
     var buildings: [Building]?
+    
+    //create rooms array
     var meetingRvars: [MeetingRoom]?
-    let apiRootUrl = "https://navsingh.org.uk/mrpro/"
+    
+    //create uisearch controller
     let searchController = UISearchController(searchResultsController: nil)
     
+    //create a variable to store current building
     var currentBuilding : Building!
     
     override func viewDidLoad() {
         super.viewDidLoad()
          self.title = "Building List"
+        
         //setting background color for the view
         view.backgroundColor = UIColor.white
+        
+        //hiding the navigation controller on swipe
         self.navigationController?.hidesBarsOnSwipe = true
         searchController.searchResultsUpdater = self
         searchController.dimsBackgroundDuringPresentation = false
         
+        //styling
         tableView.tableHeaderView = searchController.searchBar
         searchController.searchBar.placeholder = "Search by Building Name"
         searchController.searchBar.barTintColor = UIColor.black
@@ -36,23 +46,30 @@ class BuildingListTableViewController: UITableViewController, UISearchBarDelegat
         
         self.refreshControl?.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
         
-//        sendServiceRequests("")
+        //make web api request
         apiRequestForBuildings()
     }
     
+    //updating the search results
     func updateSearchResults(for searchController: UISearchController) {
+        
+        //get the user input from search bar and call the api seach building method
         if let searchString = searchController.searchBar.text {
-//            sendServiceRequests(searchString)
-//            apiRequestForBuildings()
-            apiSearchBuilding(stringToSearch: searchController.searchBar.text!)
+            
+            //call the search buildings method
+            apiSearchBuilding(stringToSearch: searchString)
         }
         else
         {
+            //if the search bar text is empty show a list of all buildings
             apiRequestForBuildings()
         }
     }
     
     
+    /*method returns building in accordance to user input - our api allow
+    /search based on three factors- Use can search by Builing Name, City or Country the
+    building is located in*/
     func apiSearchBuilding(stringToSearch: String)
     {
         var dictRequest: [String : Any] = [:]
@@ -147,18 +164,7 @@ class BuildingListTableViewController: UITableViewController, UISearchBarDelegat
         self.present(alertController, animated: true, completion: nil)
     }
     func sendServiceRequests(_ searchText: String) {
-        /**
-        sendRequest("buildings?name=\(searchText)") {
-            data in
-            let jsonParser = CustomJsonParser()
-            self.buildings = jsonParser.parseBuildingJson(data)
-            DispatchQueue.main.async {
-                [unowned self] in
-                self.tableView.reloadData()
-                self.refreshControl?.endRefreshing()
-            }
-        }
- **/
+        
         
         let path : String = Bundle.main.path(forResource: "jsonFile", ofType: "json") as String!
         let data:NSData = NSData.dataWithContentsOfMappedFile(path as String) as! NSData
@@ -169,52 +175,8 @@ class BuildingListTableViewController: UITableViewController, UISearchBarDelegat
         
     }
     
-    func sendRequest(_ serviceEndpoint: String, completion: @escaping (_ data: Data) -> ()) {
-        
-        let session = URLSession.shared
-        let request = createRequest(serviceEndpoint)
-        
-        let task = session.dataTask(with: request, completionHandler: { (data, response, error) in
-            
-            if let httpResponse = response as? HTTPURLResponse {
-                switch httpResponse.statusCode {
-                case 500..<600:
-                    print("Server error \(httpResponse.statusCode)")
-                case 400..<500:
-                    print("Client request error \(httpResponse.statusCode)")
-                default:
-                    if let data = data {
-                        completion(data)
-                    } else {
-                        print("There is no available data")
-                    }
-                }
-            } else {
-                print("Unknown error occured. Please check if your web api is up and running")
-            }
-        })
-        
-        task.resume()
-    }
-    
-    func createRequest(_ serviceEndpoint: String) -> URLRequest {
-        
-        let username = "virksaabnavjot@gmail.com"
-        let password = "Mtkh6767"
-        let loginString = NSString(format: "%@:%@", username, password)
-        let loginData: Data = loginString.data(using: String.Encoding.utf8.rawValue)!
-        let base64LoginString = loginData.base64EncodedString(options: .lineLength64Characters)
-        
-        let url = URL(string: ("\(apiRootUrl)/\(serviceEndpoint)").addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!)
-        let request = NSMutableURLRequest(url: url!)
-        request.httpMethod = "GET"
-        request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
-        
-        return request as URLRequest
-    }
     
     func handleRefresh(_ refreshControl: UIRefreshControl) {
-//        sendServiceRequests("")
         apiRequestForBuildings()
     }
     
